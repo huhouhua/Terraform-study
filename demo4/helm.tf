f6dae3035835214d0e6db37071b70f991ee73e6d
huhouhua <huhouhuam@outlook.com>
2024-09-29 03:19:44 +0800

    feat: add crossplane demo

--- demo4/helm.tf
+++ demo4/helm.tf
@@ -1,6 +1,5 @@
 resource "helm_release" "argo_cd" {
-#   depends_on       = [module.k3s, null_resource.download_k3s_yaml]
-  depends_on       = [module.k3s]
+  depends_on       = [module.k3s, null_resource.download_k3s_yaml]
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
---
+++ demo6/crossplane/tencent/variables.tf
@@ -0,0 +1,14 @@
+variable "secret_id" {
+  default = "you_secret"
+}
+
+variable "secret_key" {
+  default = "your_secret_key"
+}
+
+variable "region" {
+  default = "ap-hongkong"
+}
+variable "password" {
+  default = "password123"
+}
